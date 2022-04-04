bfe71268eefd653946eaa9a2b785955a28ab3f02
Kjetil H <haugkjet@gmail.com>
2022-04-04 09:10:52 +0200

    Initial revision

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <h1 className="text-3xl text-indigo-500 font-bold">
+      NextJS/Tailwind deployment test
+    </h1>
+  )
+}
